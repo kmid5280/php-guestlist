619adb496595fffe12fb4ba74983868a6af188d0
Kyle Middleton <kylemid321@gmail.com>
2019-05-05 17:17:04 -0600

    initial commit

---
+++ action.php
@@ -0,0 +1,26 @@
+<?php
+    require __DIR__ . '/vendor/autoload.php';
+    $dotenv = Dotenv\Dotenv::create(__DIR__);
+    $dotenv->load();
+    $servername = getenv('servername');
+    $username = getenv('username');
+    $password = getenv('password');
+    $dbname = getenv('dbname');
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "INSERT INTO MyGuests (firstname, lastname)
+        VALUES ('$firstname', '$lastname')";
+        $conn->exec($sql);
+        echo "New record created successfully with first name $firstname and last name $lastname.";
+        }
+    catch(PDOException $e)
+        {
+        echo $sql . "<br>" . $e->getMessage();
+        }
+    
+    $conn = null;
+?>
